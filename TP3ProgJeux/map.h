ae79d5b82a3281cd0ce48f54ec475b99856383b1
antoine Roy-Lachance <titoine9912@hotmail.com>
2017-11-28 08:21:52 -0500

    modif map constructeur public

--- TP3ProgJeux/map.h
+++ TP3ProgJeux/map.h
@@ -8,6 +8,7 @@
 
 class map
 {
+public:
 	map();
 	bool load_map(const char scene_path[], std::vector<boss>& bosses, std::vector<base_enemy> base_enemies,
 		std::vector<upgraded_turret> upgraded_turrets, std::vector<base_turret> base_turrets, std::vector<kamikaze> kamikazes);
