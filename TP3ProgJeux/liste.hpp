e105e57de8038ded16da76f963a685e56c920d90
antoine Roy-Lachance <titoine9912@hotmail.com>
2017-12-01 12:47:52 -0500

    modif test

--- TP3ProgJeux/liste.hpp
+++ TP3ProgJeux/liste.hpp
@@ -1,40 +1,115 @@
 #pragma once
+#include <algorithm> 
+
 template <class T>
 class Liste
 {
 	struct Box
 	{
 		T value;
-		Box* next;
-		Box* previous;
-		Box(const T& value, Box* next) :value(value), next(next) {}
-		Box(const T& value, Box* previous) :value(value), previous(previous){}
+		Box *next, *previous;
+		Box(const T& value, Box*N = nullptr, Box*P = nullptr) {}
+		~Box() { previous = next = nullptr; }
 	};
 
 	Liste(const Liste&) = delete;
 
-	Box *first, *last;
+	Box avant;
+	Box apres;
 	size_t sz;
 
+	Box* insert(Box* iterator, const T& value);
+	Box* erase(Box* iterator);
+
 public:
+	class iterator;
 	Liste();
 	~Liste();
-	void Insert(Box* iterator, T valeur);
+	Liste(const Liste& other);
+	Liste& operator=(const Liste& other);
+	void swap(Liste& other);
 
-	T& front();
+	iterator Insert(iterator pos, const T& value);
+	iterator erase(iterator pos);
+
+	void push_back(const T&);
+	void pop_back();
+	void push_front(const T&);
+	void pop_front();
+
 	T& back();
-	const T& front() const;
-	const T& back() const;
+	T& front();
 
-	bool empty() const;
-	size_t size() const;
+	void clear();
+	size_t size()const;
+	bool empty()const;
+
+	iterator begin();
+	iterator end();
+
+	void reverse();
+	void splice(iterator pos, Liste& other);
 };
+
+template <typename T>
+class Liste<T>::iterator
+{
+	friend class Liste<T>;
+private:
+	Box* POINTEUR;
+public:
+	iterator(Box*value=nullptr) :POINTEUR(value) {}
+
+	T& operator*()const
+	{
+		return &POINTEUR;
+	}
+
+	T* operator->()const { return &(POINTEUR->CONTENU); }
+
+	iterator& operator++()
+	{
+		++POINTEUR;
+		return &POINTEUR;
+	}
+	
+	iterator operator++(int)
+	{
+		POINTEUR++;
+		return POINTEUR;
+	}
+
+	iterator& operator--()
+	{
+		--POINTEUR;
+		return &POINTEUR;
+	}
+
+	iterator operator--(int)
+	{
+		POINTEUR--;
+		return POINTEUR;
+	}
+
+	bool operator==(const iterator&droite)const
+	{
+		return POINTEUR == droite.POINTEUR;
+	}
+
+	bool operator!=(const iterator&droite)const
+	{
+		return POINTEUR != droite.POINTEUR;
+	}
+};
+
+
+
 
 template<class T>
 inline Liste<T>::Liste()
 {
-	first = nullptr;
-	last = nullptr;
+	first->next = last;
+	last->previous = first;
 	sz = 0;
 }
 
@@ -45,23 +120,48 @@ inline Liste<T>::~Liste()
 }
 
 template<class T>
-inline void Liste<T>::Insert(Box* iterator, T value)
+inline Liste<T>::Liste(const Liste&)
 {
-	if (iterator->next != nullptr)
-	{
-		iterator->next = new Box(value, iterator->next->next);
-		//iterator = iterator->next;
-	}
-	else if (iterator->next == nullptr)
-	{
-		iterator->next = new Box(value, nullptr);
-		last = iterator->next;
-	}
-	else
-	{
-		iterator = last = first = new Box(value, nullptr);
-	}
-
-	sz++;
-	
+	*this = other;
 }
+
+template<class T>
+void Liste<T>::swap(Liste& other)
+{
+	std::swap(avant, other.avant);
+	std::swap(sz, other.sz);
+	std::swap(apres, other.apres);
+}
+
+template<class T>
+Box* Liste<T>::insert(Box* iterator, const T& value)
+{
+	iterator->previous->next = iterator->previous = new Box(value, iterator->previous->suivant, iterator->previous);
+	return iterator->previous;
+}
+//template<class T>
+//inline void Liste<T>::Insert(Box* iterator, T value)
+//{
+//	if (iterator->next != nullptr && iterator->previous == nullptr)
+//	{
+//		iterator = new Box(value, iterator->next);
+//	}
+
+//	else if (iterator->next != nullptr)
+//	{
+//		iterator = new Box(value, iterator->previous,iterator->next);
+//		//iterator = iterator->next;
+//	}
+//	else if (iterator->next == nullptr)
+//	{
+//		iterator = new Box(value, iterator->previous);
+//		last = iterator->next;
+//	}
+//	else
+//	{
+//		iterator = last = first = new Box(value, nullptr);
+//	}
+
+//	sz++;
+	
+//}
